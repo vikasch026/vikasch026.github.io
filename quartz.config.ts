d3013c5bee5baa74fd4435b6c027f9da4f4fd601
vikas <vkschoudhary001@gmail.com>
2026-04-04 16:19:35 +0530

    Configure custom domain kickas.in and update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "vikasch026.github.io",
+    baseUrl: "kickas.in",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
