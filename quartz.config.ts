64f27df24564b4d4a9fddf70e0788c150d736a46
vikas <vkschoudhary001@gmail.com>
2026-04-04 16:32:34 +0530

    Fix baseUrl to correct GitHub Pages URL vikasc026.github.io

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "kickas.in",
+    baseUrl: "vikasch026.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
